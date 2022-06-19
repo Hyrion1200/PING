beee2ee05144aca8e8b8ed4ab316918c819789a5
Nicolas <nicolas.charles@epita.fr>
2022-06-19 22:38:45 +0200

    feature(git_feature) - Enhancement - Push and Commit command execution - Find a way to return from functions (same for everyone ?)

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Commit.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Commit.java
@@ -1,20 +1,34 @@
 package fr.epita.assistants.myide.domain.entity.features;
 
 import fr.epita.assistants.myide.domain.entity.Feature.ExecutionReport;
+
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Project;
 
 public
 class Commit implements Feature {
   @Override public ExecutionReport execute(Project project, Object... params) {
-    // TODO Auto-generated method stub
+    // DONE
+    ProcessBuilder pb = new ProcessBuilder()
+                        .command("git", "commit", "-m", "\"" + (String) params[0] + "\"")
+                        .directory(project.getRootNode().getPath().toFile());
+
+    try {
+      Process p = pb.start();
+      int exit = p.waitFor();
+
+      if (exit != 0) {
+        throw new Exception(String.format("git commit returned %d", exit));
+      }
+      return new ExecutionReport("test", exit);
+      // implement ExecutionReport ?
+    } 
+    catch (Exception  e) {
+      e.printStackTrace();
+      return new ExecutionReport("test", -1);
+      // implement ExecutionReport ?
+    }
     throw new UnsupportedOperationException("Not supported yet."); // To
-                                                                   // change
-                                                                   // body
-                                                                   // of
-                                                                   // generated
-                                                                   // methods,
-                                                                   // choose
   }
 
   @Override public Type type() {
--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Push.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Push.java
@@ -7,14 +7,28 @@ import fr.epita.assistants.myide.domain.entity.Project;
 public
 class Push implements Feature {
   @Override public ExecutionReport execute(Project project, Object... params) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Not supported yet."); // To
-                                                                   // change
-                                                                   // body
-                                                                   // of
-                                                                   // generated
-                                                                   // methods,
-                                                                   // choose
+    // DONE
+
+    ProcessBuilder pb = new ProcessBuilder()
+                        .command("git", "push")
+                        .directory(project.getRootNode().getPath().toFile());
+
+    try {
+      Process p = pb.start();
+      int exit = p.waitFor();
+
+      if (exit != 0) {
+        throw new Exception(String.format("git push returned %d", exit));
+      }
+      return new ExecutionReport("test", exit);
+      // implement ExecutionReport ?
+    }
+    catch (Exception  e) {
+      e.printStackTrace();
+      return new ExecutionReport("test", -1);
+      // implement ExecutionReport ?
+    }
+    throw new UnsupportedOperationException("Not supported yet."); 
   }
 
   @Override public Type type() {
