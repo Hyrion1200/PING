60d6f23872b8acd5fb2156cdba33287b7b934051
Nicolas <nicolas.charles@epita.fr>
2022-06-21 00:05:42 +0200

    feat(git_feature) - Enhancement - Added add and pull feature - return has to be fixed for each function

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Add.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Add.java
@@ -7,19 +7,46 @@ import fr.epita.assistants.myide.domain.entity.Project;
 public
 class Add implements Feature {
   @Override public ExecutionReport execute(Project project, Object... params) {
-    // TODO Auto-generated method stub
+    String args = "";
+    for (Object param : params) {
+      args += param + " ";
+    }
+
+    ProcessBuilder pb = new ProcessBuilder()
+                        .command("git", "add", args);
+
+
+    try {
+      Process p = pb.start();
+      int exit = p.waitFor();
+
+      if (exit != 0) {
+        throw new Exception(String.format("git add returned %d", exit));
+      }
+      return new ExecutionReporter() {
+        @Override public boolean isSuccess() {
+          return false;
+        }
+      };
+      // implement ExecutionReport ?
+    }
+    catch (Exception  e) {
+      e.printStackTrace();
+      return new ExecutionReport("test", -1);
+      // implement ExecutionReport ?
+    }
     throw new UnsupportedOperationException("Not supported yet."); // To
-                                                                   // change
-                                                                   // body
-                                                                   // of
-                                                                   // generated
-                                                                   // methods,
-                                                                   // choose
   }
 
   @Override public Type type() {
     // Are we not supposed to change any @Given file ? If so how do I return a
     // Type.GIT ?
-    return Type.GIT;
+    return Feature.Type.GIT.ADD;
   }
+
+  class ExecutionReporter implements ExecutionReport {
+    public
+      boolean isSuccess() {
+        return false;
+      }
 }
