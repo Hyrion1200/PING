998c1b9e99c88d86fcea668c767efcf0fc8db9ff
de Souza <nathan.de-souza@epita.fr>
2022-06-21 13:46:47 +0200

    feat(maven): Added generic feature checker to features

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Exec.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Exec.java
@@ -6,27 +6,27 @@ import fr.epita.assistants.myide.domain.entity.Aspect;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
+import fr.epita.assistants.utils.Feature_Check;
 
 public
 class Exec implements Feature {
   @Override public ExecutionReport execute(Project project, Object... params) {
     // Check if the project has the Maven aspect type
-    Set<Aspect> aspects = project.getAspects();
-    for (Aspect aspect : aspects) {
-      if (aspect.getType() == Mandatory.Aspects.MAVEN) {
-        // Execute mvn exec with the given parameters
-        try {
-          final int exitCode = Runtime.getRuntime().exec("mvn exec").waitFor();
-          return new ExecutionReport() {
-            @Override public boolean isSuccess() {
-              return exitCode == 0;
-            }
-          };
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        } catch (java.io.IOException e) {
-          e.printStackTrace();
-        }
+    Feature_Check checker = new Feature_Check(project, Mandatory.Aspects.MAVEN);
+    if (checker.check()) {
+      // Execute mvn exec
+      try {
+        final int exitCode =
+            Runtime.getRuntime().exec("mvn exec " + params).waitFor();
+        return new ExecutionReport() {
+          @Override public boolean isSuccess() {
+            return exitCode == 0;
+          }
+        };
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (java.io.IOException e) {
+        e.printStackTrace();
       }
     }
     return new ExecutionReport() {
