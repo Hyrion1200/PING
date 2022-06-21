31fe19c0d5eb34e00cac03989701b640a4e89deb
Mathieu Dique <mathieu.dique@epita.fr>
2022-06-21 19:30:54 +0200

    feat(Maven Features)
    - Added ExecReport error on exceptions in maven features

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/maven/Maven_features.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/maven/Maven_features.java
@@ -3,6 +3,7 @@ package fr.epita.assistants.myide.domain.entity.features.maven;
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Project;
 import fr.epita.assistants.myide.domain.entity.features.Features;
+import fr.epita.assistants.myide.domain.entity.features.exec_report.ExecReport;
 
 public abstract class Maven_features extends Features {
     private String name;
@@ -25,22 +26,12 @@ public abstract class Maven_features extends Features {
         try {
             final int exitCode =
                     Runtime.getRuntime().exec(command.toString()).waitFor();
-            return new ExecutionReport() {
-                @Override
-                public boolean isSuccess() {
-                    return exitCode == 0;
-                }
-            };
+            return new ExecReport(ExecReport.Status.SUCCESS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
-        return new ExecutionReport() {
-            @Override
-            public boolean isSuccess() {
-                return false;
-            }
-        };
+        return new ExecReport(ExecReport.Status.ERROR, "Couldn't execute maven command");
     }
 }
