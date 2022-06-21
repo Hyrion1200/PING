d8523e8a2a534ca6c8833bef6b603ff0b772754d
hugo.hamon <hugo.hamon@epita.fr>
2022-06-21 18:12:03 +0200

    aded node Service class

--- src/main/java/fr/epita/assistants/myide/domain/service/NodeServ.java
+++ src/main/java/fr/epita/assistants/myide/domain/service/NodeServ.java
@@ -1,25 +1,102 @@
 package fr.epita.assistants.myide.domain.service;
 
+import ch.qos.logback.core.util.FileUtil;
 import fr.epita.assistants.myide.domain.entity.Node;
+import fr.epita.assistants.myide.domain.entity.Node_Entity;
+import org.apache.commons.io.FileUtils;
+
+import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static fr.epita.assistants.myide.domain.entity.Node.Types.FILE;
+import static fr.epita.assistants.myide.domain.entity.Node.Types.FOLDER;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class NodeServ implements NodeService{
+
+
     @Override
     public Node update(Node node, int from, int to, byte[] insertedContent) {
-        return null;
+        if (node.getType() == FOLDER)
+        {
+            throw new IllegalArgumentException("Node is of type folder, file expected");
+        }
+        try {
+            RandomAccessFile file = new RandomAccessFile(node.getPath().toFile(), "rw");
+            file.seek(from);
+            file.write(insertedContent);
+            return node;
+        }
+        catch (FileNotFoundException e){
+            throw new IllegalArgumentException("File could not be opened");
+        }
+        catch (IOException e){
+            throw new IllegalArgumentException("from is less than 0 or I/O error");
+        }
     }
 
     @Override
     public boolean delete(Node node) {
-        return false;
+        if (node.getType() == FOLDER){
+            List<Node> children = node.getChildren();
+            for (Node child : children){
+                this.delete(node);
+            }
+            try{
+                FileUtils.deleteDirectory(new File(node.getPath().toString()));
+                return true;
+            }
+            catch (IOException e){
+                return false;
+            }
+        }
+        File file = new File(node.getPath().toString());
+        return file.delete();
     }
 
     @Override
     public Node create(Node folder, String name, Node.Type type) {
-        return null;
+        String path = folder.getPath().toString() + "/" + name;
+        Path paths = Path.of(path);
+        try {
+            if (type == FOLDER)
+                Files.createDirectories( paths);
+            else
+                Files.createFile(paths);
+        }
+        catch (IOException e){
+            throw new IllegalArgumentException("IO Error");
+        }
+        Node NewNode = new Node_Entity( paths,type,null);
+        return NewNode;
     }
 
     @Override
     public Node move(Node nodeToMove, Node destinationFolder) {
-        return null;
+        if (destinationFolder.getType() != FOLDER)
+            throw new IllegalArgumentException("Destination is not a folder");
+        try{
+            if (nodeToMove.getType() != FILE){
+                List<Node> children = nodeToMove.getChildren();
+                for (Node child : children){
+                    this.move(child,destinationFolder);
+                }
+            }
+            else {
+                Files.move(nodeToMove.getPath(), destinationFolder.getPath(), REPLACE_EXISTING);
+            }
+        }
+        catch (IOException e) {
+            throw new IllegalArgumentException("IO error when moving a file");
+        }
+        String newpath = destinationFolder.getPath().toString() + nodeToMove.getPath().getFileName().toString();
+        return new Node_Entity(Paths.get(newpath),nodeToMove.getType(),nodeToMove.getChildren());
     }
 }
