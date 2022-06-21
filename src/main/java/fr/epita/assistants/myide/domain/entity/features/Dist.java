7bee6ea99d01fa594a2e6dbdc9766db2dec28e76
Mathieu Dique <mathieu.dique@epita.fr>
2022-06-21 18:17:52 +0200

    refactor
    - Modified indentation in Dist.java

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Dist.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Dist.java
@@ -22,58 +22,58 @@ import java.util.zip.ZipEntry;
 
 public
 class Dist extends Features {
-  // - Constructor
-  public Dist() {
+    // - Constructor
+    public Dist() {
     super(FeaturesTypes.Any.DIST);
   }
 
-  // - Private methods
-  private void zipFolder(Node rootNode) throws IOException {
-	  String zipName = FileNameUtils.getBaseName(rootNode.getPath().toString()) + ".zip";
-	  ZipArchiveOutputStream archive = new ZipArchiveOutputStream(new FileOutputStream(zipName));
+    // - Private methods
+    private void zipFolder(Node rootNode) throws IOException {
+	    String zipName = FileNameUtils.getBaseName(rootNode.getPath().toString()) + ".zip";
+		ZipArchiveOutputStream archive = new ZipArchiveOutputStream(new FileOutputStream(zipName));
 
-	  archive.setLevel(Deflater.BEST_SPEED);
-	  archive.setMethod(ZipEntry.DEFLATED);
+		archive.setLevel(Deflater.BEST_SPEED);
+		archive.setMethod(ZipEntry.DEFLATED);
 
-	  File direcotryToArchive = new File(rootNode.getPath().toString());
+		File direcotryToArchive = new File(rootNode.getPath().toString());
 
-	  for (Path path : Files.walk(direcotryToArchive.toPath()).toList()) {
-		  File file = path.toFile();
+		for (Path path : Files.walk(direcotryToArchive.toPath()).toList()) {
+		    File file = path.toFile();
 
-		  if (!file.isDirectory()) {
-			  ZipArchiveEntry entry = new ZipArchiveEntry(file, file.toString());
-			  FileInputStream fis = new FileInputStream(file);
+		    if (!file.isDirectory()) {
+		        ZipArchiveEntry entry = new ZipArchiveEntry(file, file.toString());
+		        FileInputStream fis = new FileInputStream(file);
 
-			  archive.putArchiveEntry(entry);
-			  IOUtils.copy(fis, archive);
-			  archive.closeArchiveEntry();
-		  }
-	  }
-  }
+		        archive.putArchiveEntry(entry);
+		        IOUtils.copy(fis, archive);
+		        archive.closeArchiveEntry();
+		    }
+		}
+    }
 
-  private boolean callCleanUp(Project project) {
-	  Feature cleanup = null;
+    private boolean callCleanUp(Project project) {
+	    Feature cleanup = null;
 
-	  try {
-		  cleanup = project.getFeature(Mandatory.Features.Any.CLEANUP).get();
-	  } catch (NoSuchElementException e) {
-		  return false;
-	  }
+	    try {
+		    cleanup = project.getFeature(Mandatory.Features.Any.CLEANUP).get();
+	    } catch (NoSuchElementException e) {
+		    return false;
+	    }
 
-	  return cleanup.execute(project).isSuccess();
-  }
+	    return cleanup.execute(project).isSuccess();
+    }
 
-  // - Execute function implementation
-  @Override public ExecutionReport execute(Project project, Object... params) {
-	  if (!callCleanUp(project))
-		  return new ExecReport(ExecReport.Status.ERROR, "Couldn't cleanup the project");
+    // - Execute function implementation
+	@Override public ExecutionReport execute(Project project, Object... params) {
+	    if (!callCleanUp(project))
+		    return new ExecReport(ExecReport.Status.ERROR, "Couldn't cleanup the project");
 
-	  try {
-		  zipFolder(project.getRootNode());
-	  } catch (IOException e) {
-		  return new ExecReport(ExecReport.Status.ERROR, "Couldn't create the archive");
-	  }
+	    try {
+		    zipFolder(project.getRootNode());
+	    } catch (IOException e) {
+		    return new ExecReport(ExecReport.Status.ERROR, "Couldn't create the archive");
+	    }
 
-	  return new ExecReport(ExecReport.Status.SUCCESS);
-  }
+	    return new ExecReport(ExecReport.Status.SUCCESS);
+    }
 }
