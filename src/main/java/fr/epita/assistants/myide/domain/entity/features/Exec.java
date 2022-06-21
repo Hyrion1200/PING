0fe0c94f9d4032c26e14f014b916f5c57cd59b11
de Souza <nathan.de-souza@epita.fr>
2022-06-21 12:45:45 +0200

    feat(maven): Handling bad aspect for features idea

--- src/main/java/fr/epita/assistants/myide/domain/entity/features/Exec.java
+++ src/main/java/fr/epita/assistants/myide/domain/entity/features/Exec.java
@@ -1,24 +1,33 @@
 package fr.epita.assistants.myide.domain.entity.features;
 
+import java.util.Set;
+
+import fr.epita.assistants.myide.domain.entity.Aspect;
 import fr.epita.assistants.myide.domain.entity.Feature;
-import fr.epita.assistants.myide.domain.entity.Feature.ExecutionReport;
+import fr.epita.assistants.myide.domain.entity.Mandatory;
 import fr.epita.assistants.myide.domain.entity.Project;
 
 public
 class Exec implements Feature {
   @Override public ExecutionReport execute(Project project, Object... params) {
-    // Execute mvn exec
-    try {
-      final int exitCode = Runtime.getRuntime().exec("mvn exec").waitFor();
-      return new ExecutionReport() {
-        @Override public boolean isSuccess() {
-          return exitCode == 0;
+    // Check if the project has the Maven aspect type
+    Set<Aspect> aspects = project.getAspects();
+    for (Aspect aspect : aspects) {
+      if (aspect.getType() == Mandatory.Aspects.MAVEN) {
+        // Execute mvn exec with the given parameters
+        try {
+          final int exitCode = Runtime.getRuntime().exec("mvn exec").waitFor();
+          return new ExecutionReport() {
+            @Override public boolean isSuccess() {
+              return exitCode == 0;
+            }
+          };
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } catch (java.io.IOException e) {
+          e.printStackTrace();
         }
-      };
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (java.io.IOException e) {
-      e.printStackTrace();
+      }
     }
     return new ExecutionReport() {
       @Override public boolean isSuccess() {
@@ -30,6 +39,6 @@ class Exec implements Feature {
   @Override public Type type() {
     // Are we not supposed to change any @Given file ? If so how do I return a
     // Type.MAVEN ?
-    return Type.MAVEN;
+    return Mandatory.Features.Maven.EXEC;
   }
 }
