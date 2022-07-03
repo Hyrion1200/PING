28c11a58ba1588845905e5bcd0dc16306f0b9565
Baptiste Nehmé <baptiste.nehme@epita.fr>
2022-07-03 22:53:41 +0200

    feat: basic tree without resize, store, and backend

---
+++ frontend/src/components/Tree/File.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let name;
+    // TODO - use store for settings as the theme
+    export let theme = "default";
+    $: type = name.slice(name.lastIndexOf(".") + 1);
+</script>
+
+<span
+    style="background-image: url(/src/assets/themes/{theme}/icons/{type}.svg), url(/src/assets/themes/{theme}/icons/file.svg)"
+    >{name}</span
+>
+
+<style>
+    span {
+        padding: 0 0 0 1.5em;
+        background: 0 0.1em no-repeat;
+        background-size: 1em 1em;
+    }
+</style>
---
+++ frontend/src/components/Tree/Folder.svelte
@@ -0,0 +1,53 @@
+<script>
+    import File from "./File.svelte";
+
+    export let expanded = false;
+    export let name;
+    export let files;
+
+    function toggle() {
+        expanded = !expanded;
+    }
+</script>
+
+<span class:expanded on:click={toggle}>{name}</span>
+
+{#if expanded}
+    <ul>
+        {#each files as file}
+            <li>
+                {#if file.files}
+                    <svelte:self {...file} />
+                {:else}
+                    <File {...file} />
+                {/if}
+            </li>
+        {/each}
+    </ul>
+{/if}
+
+<!-- TODO background image use variables -->
+<style>
+    span {
+        padding: 0 0 0 1.5em;
+        background: url(/src/assets/themes/default/icons/folder.svg) 0 0.1em
+            no-repeat;
+        background-size: 1em 1em;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    .expanded {
+        background-image: url(/src/assets/themes/default/icons/opened-folder.svg);
+    }
+
+    ul {
+        padding: 0.2em 0 0 0.5em;
+        margin: 0 0 0 0.5em;
+        list-style: none;
+    }
+
+    li {
+        padding: 0.2em 0;
+    }
+</style>
