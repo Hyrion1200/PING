7d5cdcb98864f9bba7f059e6cd569b228c331c39
Baptiste Nehmé <baptiste.nehme@epita.fr>
2022-07-04 22:18:05 +0200

    fix: use localhost:8080 as BASE_URL if launched with vite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import App from './App.svelte'
 
+if (window.BASE_URL === undefined) {
+  window.BASE_URL = 'http://localhost:8080';
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
