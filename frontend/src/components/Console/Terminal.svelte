90c1fd9ad6d2c3843cbef0232182977152ba631e
hugo.hamon <hugo.hamon@epita.fr>
2022-07-05 23:31:26 +0200

    trying some stuff not really working at the moment but you know we will get there eventually

--- frontend/src/components/Console/Terminal.svelte
+++ frontend/src/components/Console/Terminal.svelte
@@ -2,24 +2,51 @@
 <script src="./node_modules/xterm/lib/xterm.js">
     import {show_terminal} from "../../stores/OutputStore"
     import { Terminal } from 'xterm';
+    import { FitAddon } from 'xterm-addon-fit';
+    import { tick } from "svelte";
 
     let show = false;
-    let Prompt = "/home/hugo ~ Prompt";
+    let Prompt = "~ ";
 
     show_terminal.subscribe((value) => {
         show = value;
     })
 
-    //var term = new Terminal();
+    var command = "";
+    const fitAddon = new FitAddon();
+    async function create_term(){
+        await tick();
+        var term = new Terminal(
+            {
+                rendererType: "dom",
+            }
+        );
+        term.open(document.getElementById('Terminal'));
+        term.loadAddon(fitAddon);
+        term.write(Prompt)
+        fitAddon.fit();
+        term.onData((data) => {
+            if (data == "\r") 
+            {
+                console.log(command);
+                command = "";
+                term.write(`\n\r${Prompt}`);
+            }
+             else 
+             {
+                command += data;
+                term.write(data);
+             }
+        })
+    }
+    create_term();
 </script>
 
 
 {#if show}
-    <div>
-        <p id="Terminal">
-            {Prompt}
-        </p>
+    <div id="Terminal">
     </div>
+    {create_term()}
 {/if}
 
 <style>
@@ -28,15 +55,5 @@
         position: absolute;
         height: 20%;
         width: 71.4%;
-    }
-    p {
-        width: 100%;
-        font-size: 15px;
-        border: none;
-        resize: none;
-        margin: 0;
-        background-color: #2d2d2d;
-        color: white;
-        outline: none;
     }
 </style>
