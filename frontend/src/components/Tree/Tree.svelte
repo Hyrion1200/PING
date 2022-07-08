915de90718b28324646815074abdaa2ea33ede59
Baptiste Nehmé <baptiste.nehme@epita.fr>
2022-07-08 18:08:11 +0200

    feat: reload on changes working.
    - Update of tree need user interaction.
    - Settings not load on update

--- frontend/src/components/Tree/Tree.svelte
+++ frontend/src/components/Tree/Tree.svelte
@@ -27,6 +27,110 @@
         return { name, path: root.path.slice(7), relativePath }; // Remove file:// before absolute path
     }
 
+    let root = undefined;
+
+    // Add element to the root, and all necessary intermediate directories
+    function addToRoot(path, isDir) {
+        if (root === undefined) return;
+
+        let current = root;
+        let pathSplitted = path
+            .split(root.name)[1]
+            .replace(/^\/+|\/+$/g, "")
+            .split("/");
+        for (let i = 0; i < pathSplitted.length - 1; i++) {
+            let name = pathSplitted[i];
+            if (current.children) {
+                // On a Folder
+                let child = current.children.find(
+                    (child) => child.name === name
+                );
+                if (child === undefined) {
+                    child = {
+                        name,
+                        children: [],
+                    };
+                    current.children.push(child);
+                }
+                current = child;
+            } else {
+                // Not supposed to happen, it means that a file became a directory
+                console.error("File became a directory (AddToRoot)");
+            }
+        }
+
+        // All the path has been either checked or created up to the final file / directory to create
+        if (current.children) {
+            // On a Folder
+            let child = current.children.find(
+                (child) => child.name === pathSplitted[pathSplitted.length - 1]
+            );
+            if (child === undefined) {
+                if (isDir) {
+                    child = {
+                        name: pathSplitted[pathSplitted.length - 1],
+                        children: [],
+                    };
+                } else {
+                    child = {
+                        name: pathSplitted[pathSplitted.length - 1],
+                        path: path,
+                        relativePath: pathSplitted.join("/"),
+                    };
+                }
+                console.debug("Created child file:", child);
+                current.children.push(child);
+            } else {
+                console.error("File already existed (AddToRoot)");
+            }
+        } else {
+            // Not supposed to happen, it means that a file became a directory
+            console.error("File became a directory (AddToRoot end)");
+        }
+    }
+
+    function removeFromRoot(path) {
+        if (root === undefined) return;
+
+        let current = root;
+        let pathSplitted = path
+            .split(root.name)[1]
+            .replace(/^\/+|\/+$/g, "")
+            .split("/");
+        for (let i = 0; i < pathSplitted.length - 1; i++) {
+            let name = pathSplitted[i];
+            if (current.children) {
+                // On a Folder
+                let child = current.children.find(
+                    (child) => child.name === name
+                );
+                if (child === undefined) {
+                    child = {
+                        name,
+                        children: [],
+                    };
+                    current.children.push(child);
+                }
+                current = child;
+            } else {
+                // Not supposed to happen, it means that a file became a directory
+                console.error("File became a directory (RemoveFromRoot)");
+            }
+        }
+
+        const index = current.children.findIndex(
+            (child) => child.name === pathSplitted[pathSplitted.length - 1]
+        );
+        if (current.children && index !== -1) {
+            current.children.splice(index, 1);
+        } else {
+            // Not supposed to happen, it means that a file became a directory
+            console.error(
+                "Last directory of path does not exists (RemoveFromRoot end)"
+            );
+        }
+    }
+
     let watcher = undefined;
 
     async function watchRoot() {
@@ -35,11 +139,25 @@
 
         watcher.onmessage = (event) => {
             const data = JSON.parse(event.data);
-            console.debug(data);
+            // console.debug(data);
+            // FIXME : backend does not detect subdirectory on creation
+            // (mkdir -p a/b/c => CREATE a)
+            // (mv fullDir . => CREATE fullDir)
+            if (data.type === "ENTRY_MODIFY") {
+                // TODO : handle file modification on disk
+                console.debug("ENTRY_MODIFY", data);
+            } else if (data.type === "ENTRY_CREATE") {
+                const fullPath = data.path;
+                const isDir = data.folder;
+                addToRoot(fullPath, isDir);
+            } else if (data.type === "ENTRY_DELETE") {
+                const fullPath = data.path;
+                removeFromRoot(fullPath);
+            } else {
+                console.debug("Ignoring event:", data);
+            }
         };
     }
-
-    let root = undefined;
 
     $: {
         if ($project !== undefined) {
