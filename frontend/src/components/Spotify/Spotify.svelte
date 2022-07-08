bc194c775cef1b39ca7eaf7cc9b58b2fb30500f5
de Souza <nathan.de-souza@epita.fr>
2022-07-08 23:01:34 +0200

    feat(Spotify): Handle spotify playlist with button to switch

--- frontend/src/components/Spotify/Spotify.svelte
+++ frontend/src/components/Spotify/Spotify.svelte
@@ -1,12 +1,63 @@
-<div>
+<script>
+    import Popup from "../Popup/Popup.svelte";
+    import { Spotify } from "sveltekit-embed";
+    let popup;
+
+    async function handle_switchPlaylist()
+    {
+        let path = popup.answer;
+        if (!path.includes("embed"))
+        {
+            //https://open.spotify.com/playlist/2XkyTxH09YORbTc8vG3POn
+            let arr = path.slice(0, 25) + "embed/" + path.slice(25);
+            path = arr;
+        }
+        console.log(path)
+        document.getElementsByClassName("spotify-sveltekit-embed")[0].setAttribute("src", path);
+    }
+
+    function askPlaylistPath()
+    {
+        popup.prompt(handle_switchPlaylist);
+    }
+
+    function disableSpotify()
+    {
+        document.getElementsByClassName("spotify-sveltekit-embed")[0].setAttribute("show", "false");
+        console.log("Loaded");
+    }
+</script>
+<!-- svelte-ignore a11y-missing-attribute -->
+<div on:load|once={disableSpotify}>
     <h2>Spotify</h2>
+<Spotify
+    spotifyLink="playlist/3UDD3bVqCZWAoF6LhCwZ68"
+    ></Spotify>
+<button id="open" on:click={askPlaylistPath} >Change Playlist</button>
 </div>
 
+<Popup bind:this={popup} sentence="Enter playlist link: " />
+
+
 <style>
-    h2 {
+    button {
+        cursor: pointer;
+        border: none;
+        background-color: #202020;
         color: white;
+        padding: 10px;
+        width: 100%;
+    }
+
+    button:hover {
+        background-color: #3d3d3d;
+    }
+
+    h2 {
+        color:white;
         text-align: center;
     }
+
     div {
         min-width: 250px;
         background-color: #121212;
