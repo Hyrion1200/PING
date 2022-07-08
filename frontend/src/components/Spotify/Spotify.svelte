9e70509a53f86ee56bb52eaa116a4d7677d4104c
de Souza <nathan.de-souza@epita.fr>
2022-07-08 23:59:19 +0200

    feat(Spotify): Improvement and without pre loaded playlist

--- frontend/src/components/Spotify/Spotify.svelte
+++ frontend/src/components/Spotify/Spotify.svelte
@@ -2,37 +2,47 @@
     import Popup from "../Popup/Popup.svelte";
     import { Spotify } from "sveltekit-embed";
     let popup;
+    let load = false;
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
 
     async function handle_switchPlaylist()
     {
+        console.log(document.getElementsByClassName("spotify-sveltekit-embed"))
         let path = popup.answer;
         if (!path.includes("embed"))
         {
-            //https://open.spotify.com/playlist/2XkyTxH09YORbTc8vG3POn
+            // Ex : https://open.spotify.com/playlist/2XkyTxH09YORbTc8vG3POn
+
+            // Insert "embed" after open.spotify.com/ to prevent cors error
             let arr = path.slice(0, 25) + "embed/" + path.slice(25);
             path = arr;
         }
+
         console.log(path)
         document.getElementsByClassName("spotify-sveltekit-embed")[0].setAttribute("src", path);
+
+        if (!load)
+        {
+            await delay(1000); // To prevent bad request from showing
+            load = true;
+        }
+        document.getElementById("spotify").style.visibility = "visible";
     }
 
     function askPlaylistPath()
     {
         popup.prompt(handle_switchPlaylist);
     }
-
-    function disableSpotify()
-    {
-        document.getElementsByClassName("spotify-sveltekit-embed")[0].setAttribute("show", "false");
-        console.log("Loaded");
-    }
 </script>
-<!-- svelte-ignore a11y-missing-attribute -->
-<div on:load|once={disableSpotify}>
+
+<div>
     <h2>Spotify</h2>
+<div id=spotify>
 <Spotify
-    spotifyLink="playlist/3UDD3bVqCZWAoF6LhCwZ68"
+    spotifyLink=""
     ></Spotify>
+</div>
 <button id="open" on:click={askPlaylistPath} >Change Playlist</button>
 </div>
 
@@ -56,6 +66,10 @@
     h2 {
         color:white;
         text-align: center;
+    }
+    #spotify
+    {
+        visibility: hidden;
     }
 
     div {
